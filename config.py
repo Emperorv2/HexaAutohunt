0af830c6d18d2c105d6178080ad6cb40df790c3d
Emperorv2 <161468144+Emperorv2@users.noreply.github.com>
2025-03-09 21:50:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 # Telegram API credentials
-   api_id = 2282111  # Replace with your API ID
-   api_hash = 'da58a1841a16c352a2a999171bbabcad'  # Replace with your API Hash
+api_id = 21293406  # Your API ID
+api_hash = 'dbaa72e10301db2908f08347c6333602'  # Your API Hash
 
-   # Phone number for authentication
-   phone_number = '+1234567890'  # Replace with your phone number
+# Phone number for authentication
+phone_number = '+251716958387'  # Your phone number in international format
 
-   # Chat ID
-   chat = 572621020  # Replace with your chat ID
+# Chat ID
+chat = 1002433318417  # Your chat ID
 
-   # List of Pokémon to catch during auto-hunt
-   pokemon_to_catch = [
-       "Eternatus", "Zacian", "Dialga", "Palkia", "Mewtwo", "Arceus", "Zamazenta",
-       "Glastrier", "Calyrex", "Kyurem", "Lunala", "Necrozma", "Rayquaza",
-       "Cosmoem", "Yveltal", "Kyogre", "Xerneas", "Cosmog", "Groudon", "Giratina",
-       "Zeraora", "Marshadow", "Buzzwole", "Solgaleo", "Zyagrde", "Slaking",
-       "Metagross", "Beldum", "Slakoth", "Vigoroth", "Garchomp", "Gallade",
-       "Gardevoir", "Dragapult", "Ho-oh", "Lugia", "Reshiram", "Regigigas",
-       "Gengar", "Alakazam", "Greninja", "Salamance", "Charizard", "Blastoise",
-       "Lucario", "Darmanitan"
-   ]
+# List of Pokémon to catch during auto-hunt
+pokemon_to_catch = [
+    "Eternatus", "Zacian", "Dialga", "Palkia", "Mewtwo", "Arceus", "Zamazenta",
+    "Glastrier", "Calyrex", "Kyurem", "Lunala", "Necrozma", "Rayquaza",
+    "Cosmoem", "Yveltal", "Kyogre", "Xerneas", "Cosmog", "Groudon", "Giratina",
+    "Zeraora", "Marshadow", "Buzzwole", "Solgaleo", "Zyagrde", "Slaking",
+    "Metagross", "Beldum", "Slakoth", "Vigoroth", "Garchomp", "Gallade",
+    "Gardevoir", "Dragapult", "Ho-oh", "Lugia", "Reshiram", "Regigigas",
+    "Gengar", "Alakazam", "Greninja", "Salamance", "Charizard", "Blastoise",
+    "Lucario", "Darmanitan"
+]
