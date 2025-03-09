84c5b3a2d92f1986a08bf733e5d66392d021b39a
Emperorv2 <161468144+Emperorv2@users.noreply.github.com>
2025-03-09 21:11:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# Telegram API credentials
+  api_id = 2282111  # Replace with your API ID
+  api_hash = 'da58a1841a16c352a2a999171bbabcad'  # Replace with your API Hash
+
+  # Chat ID
+  chat = 572621020  # Replace with your chat ID
+
+  # List of Pokémon to battle
+  pokemon_list = [
+      "Eternatus", "Zacian", "Dialga", "Palkia", "Mewtwo", "Arceus", "Zamazenta", 
+      "Glastrier", "Calyrex", "Kyurem", "Lunala", "Necrozma", "Rayquaza", 
+      "Cosmoem", "Yveltal", "Kyogre", "Xerneas", "Cosmog", "Groudon", "Giratina", 
+      "Zeraora", "Marshadow", "Buzzwole", "Solgaleo", "Zyagrde", "Slaking", 
+      "Metagross", "Beldum", "Slakoth", "Vigoroth", "Garchomp", "Gallade", 
+      "Gardevoir", "Dragapult", "Ho-oh", "Lugia", "Reshiram", "Regigigas", 
+      "Gengar", "Alakazam", "Greninja", "Salamance", "Charizard", "Blastoise", 
+      "Lucario", "Darmanitan"
+  ]
