94ed56f60c5a287ef0c1fbddb228f345ee416573
Emperorv2 <161468144+Emperorv2@users.noreply.github.com>
2025-03-09 21:12:04 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Command patterns
+  START_COMMAND = '/go'
+  STOP_COMMAND = '/stop'
+  HUNT_COMMAND = '/hunt'
+
+  # Button texts
+  BATTLE_BUTTON = "Battle"
+  POKE_BALLS_BUTTON = "Poke Balls"
+  ULTRA_BUTTON = "Ultra"
